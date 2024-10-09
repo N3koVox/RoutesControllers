d67d58711a66bdd7b9f3c60f1a394071d2463f6e
Bryan Leal <36249273-bryancocav@users.noreply.replit.com>
2024-10-09 23:45:49 +0000

    update para helpers e camadas

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 // Substitua pela sua string de conexão do Render.com
-const connectionString = 'postgresql://n_kj51_user:Ox9OAhSxJmyfwDxL8r32i7bxXCg5oDS2@dpg-cr7qs4d6l47c73bq6asg-a.oregon-postgres.render.com/n_kj51';
+const connectionString = 'postgresql://backend_ugaz_user:87KlrSzD5GQ0M8KBs5SaJ0bJgGj5112g@dpg-cs2rppbv2p9s738nq950-a.oregon-postgres.render.com/backend_ugaz';
 
 const pool = new Pool({
   connectionString,
